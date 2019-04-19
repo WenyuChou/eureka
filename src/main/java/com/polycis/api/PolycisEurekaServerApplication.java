761610fec1797fa884b40ca3b5e881540676b96b
656698338@qq.com <19871224wto>
2019-04-19 09:38:36 +0800

    代码初始化

---
+++ src/main/java/com/polycis/api/PolycisEurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.polycis.api;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class PolycisEurekaServerApplication extends SpringBootServletInitializer{
+	@Override
+	protected SpringApplicationBuilder configure(
+			SpringApplicationBuilder application) {
+		return application.sources(PolycisEurekaServerApplication.class);
+	}
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(PolycisEurekaServerApplication.class).web(true).run(args);
+	}
+
+}
