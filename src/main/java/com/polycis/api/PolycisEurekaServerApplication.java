3a2764c8c89277c44201792baa1d8fa2b3d0463f
656698338@qq.com <19871224wto>
2019-04-19 13:50:59 +0800

    配置注册中心

--- src/main/java/com/polycis/api/PolycisEurekaServerApplication.java
+++ src/main/java/com/polycis/api/PolycisEurekaServerApplication.java
@@ -1,6 +1,5 @@
 package com.polycis.api;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
